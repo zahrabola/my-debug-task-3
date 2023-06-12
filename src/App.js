6df3673645278fb0ea361d82a88eedf50eb64173
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-06-12 20:09:02 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodoItem, setNewTodoitem]= useState("")
+
+  const handleAddTodo = (event) => {
+    
+    ///const newTodoItem = event.target.value;
+    const newTodoList = [...todos, newTodoItem];
+
+    setTodos(newTodoList);
+  };
+const handleinputChange = (event) => {
+  setNewTodoitem(event.target.value)
+  ///console.log(event.target.value)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Debugging Challenge - 3</h1>
+      <div>
+        <input name="todo" placeholder="Type TODO description" onChange={handleinputChange} value={newTodoItem}/>
+        <button onClick={handleAddTodo}>ADD TODO</button>
+      </div>
+      <div>
+        <h3>List of Todos</h3>
+        {todos.length === 0 ? (
+          <p>None</p>
+        ) : (
+          <ul>
+            {todos.map((todo, idx) => (
+              // NOTE - Using list indexes as keys is not the ideal approach. See here - https://reactjs.org/docs/lists-and-keys.html
+              // Index works for us here as we don't expect the order of items to change
+              <li key={idx}>{todo}</li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
 
 export default App;
+/*
+ * React Debugging Challenge 3 Debrief
+ * By: Nabhan Abdulla P V (nabhanabdullapv@gmail.com)
+ *
+ * Challenge Deconstruction Video will be out on Sundays!
+ * Stay tuned to updates at https://www.youtube.com/channel/UCwF_G-nRCIiigNnojgAgs5Q
+ *
+ 
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = (event) => {
+    const newTodoItem = event.target.value;
+    const newTodoList = [...todos, newTodoItem];
+
+    setTodos(newTodoList);
+  };
+
+  return (
+    <div className="App">
+      <h1>React Debugging Challenge - 3</h1>
+      <div>
+        <input name="todo" placeholder="Type TODO description" />
+        <button onClick={handleAddTodo}>ADD TODO</button>
+      </div>
+      <div>
+        <h3>List of Todos</h3>
+        {todos.length === 0 ? (
+          <p>None</p>
+        ) : (
+          <ul>
+            {todos.map((todo, idx) => (
+              // NOTE - Using list indexes as keys is not the ideal approach. See here - https://reactjs.org/docs/lists-and-keys.html
+              // Index works for us here as we don't expect the order of items to change
+              <li key={idx}>{todo}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+}
+ */
